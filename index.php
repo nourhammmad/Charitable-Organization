1bf36676274311c12cbab291c0796a25267e38eb
NohaTarek28 <104400629+NohaTarek28@users.noreply.github.com>
2024-11-16 14:54:36 +0200

    pulli

--- index.php
+++ index.php
@@ -7,6 +7,7 @@ require_once "../Charitable-Organization/controllers/OrganizationController.php"
 require_once "../Charitable-Organization/models/UserModel.php";
 
 
+
 $db = Database::getInstance();
 Populate::populate();
 
@@ -17,5 +18,15 @@ if (Database::get_connection()) {
 } else {
     echo "Failed to connect to the database.";
 }
+
+$registerUser = new RegisterUser("volunteer@example.com", "VolunteerName", "Volunteer");
+$registerUser->signUp();
+
+// Test: Display available events for a volunteer with ID 1
+$volunteerController = new VolunteerController(1);
+$volunteerController->displayAvailableEvents();
+
+// Test: Apply for an event with ID 2 for the volunteer with ID 1
+$volunteerController->applyForEvent(2);
 
 ?>
