8f202f95efdf49db2311bded72586b0b6bf0c41d
ShahdRamzy <shahd.ramzy123@gmail.com>
2025-01-17 18:09:59 +0200

    proxy refactoring

--- Database.php
+++ Database.php
@@ -2,7 +2,7 @@
 
 // Avoid re-defining constants
 if (!defined('DB_HOST')) define('DB_HOST', 'localhost');
-if (!defined('DB_PORT')) define('DB_PORT', 3306);
+if (!defined('DB_PORT')) define('DB_PORT', 3309);
 if (!defined('DB_USER')) define('DB_USER', 'root');
 if (!defined('DB_PASS')) define('DB_PASS', '');
 if (!defined('DB_NAME')) define('DB_NAME', 'charity_db');
